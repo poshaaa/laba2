a6002d409b84671619c2a9e6b1892660aed6330d
poshaaa <Polinapolinina2710@mail.ru>
2023-04-27 20:22:38 +0400

    Добавьте файлы проекта.

---
+++ лаба2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using лаба2.Models;
+
+namespace лаба2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string id, DateTime data, string ownername, string phonenumber, string servicename, string adress, string plusservice, string review)
+        {
+            string authData = $"Новый заказ успешно добавлен: " +
+                $"ID: {id}  Дата заказа: {data}  ФИО заказчика: {ownername} Номер телефона: {phonenumber} Название услуги: {servicename} Адрес: {adress}  Доп услуги(да/нет): {Convert.ToString(plusservice)} Оценка: {Convert.ToString(review)} ";
+            return Content(authData);
+        }
+        //public IActionResult details()
+        //{
+        //    return View(User);
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ лаба2/Models/dataset.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace лаба2.Models
+{
+    [DisplayName("новый заказ")]
+    public class dataset
+    {
+        public string? id { get; set; }
+
+        [DisplayName("Дата")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? data { get; set; }
+
+        [DisplayName("ФИО заказчика")]
+        public string? ownername { get; set; }
+
+        [DisplayName("Номер телефона")]
+        public string? phonenumber { get; set; }
+
+        [DisplayName("Название услуги")]
+        public string? servicename { get; set; }
+
+        [DisplayName("Адрес")]
+        public string? adress { get; set; }
+
+        [DisplayName("Доп услуги")]
+        public bool plusservice { get; set; }
+
+        [DisplayName("Отзыв")]
+        public int review { get; set; }
+
+    }
+
+    public enum plusservice
+    {
+        not_used,
+        used
+
+    }
+    public enum servicename
+    {
+        cleaning,
+        vacuum,
+        laundry,
+        ironing
+    }
+
+    public enum review
+    {
+        super,
+        good,
+        normal,
+        bad,
+        awful
+    }
+}
